1471eba669614044e5dc505483df943c10cc5571
ldahlberg <ldahlberg@twrk.com>
2026-01-23 12:19:36 -0800

    Add missing files

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+import './App.css'
+import { AddProduct } from './product/AddProduct'
+import { ListProducts } from './product/ListProducts'
+
+function App() {
+
+  return (
+    <div>
+      <AddProduct />
+      <ListProducts />
+    </div>
+  )
+}
+
+export default App
